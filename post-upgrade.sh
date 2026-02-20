72e93c02bcf5c5aa0381075bc25dfae34daacb9b
rerulon <serveladik@yandex.com>
2026-02-20 23:32:29 +0300

    Update post-upgrade.sh

--- post-upgrade.sh
+++ post-upgrade.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 # Скрипт восстановления среды после апгрейда OpenWrt 25.12+
-echo "=== Обновляем список пакетов ==="
+echo "Обновляем список пакетов"
 apk update
 
-echo "=== Апгрейдим все пакеты ==="
+echo "Апгрейдим все пакеты"
 apk upgrade
 
-echo "=== [0/8] Устанавливаем нужные для работы пакеты ==="
+echo "[0/8] Устанавливаем нужные для работы пакеты"
 apk add --no-cache jq openssh-sftp-server coreutils-timeout curl
 
-echo "=== [1/8] Скачивание и распаковка архива payload.tar ==="
+echo "[1/8] Скачивание и распаковка архива payload.tar"
 mkdir -p /tmp/rool
 wget -qO /tmp/rool/payload.tar https://github.com/rerulon/ax6000/raw/refs/heads/main/payload.tar
 
-echo "=== [2/8] Распаковка архива payload.tar ==="
+echo "[2/8] Распаковка архива payload.tar"
 tar -xf /tmp/rool/payload.tar -C /tmp/rool/
 
-echo "=== [3/8] Копирование profile и banner в /etc ==="
+echo "[3/8] Копирование profile и banner в /etc"
 cp -f /tmp/rool/profile /etc/profile
 cp -f /tmp/rool/banner /etc/banner
 
-echo "=== [4/8] Копирование скриптов в /etc/rool и выдача прав ==="
+echo "[4/8] Копирование скриптов в /etc/rool и выдача прав"
 mkdir -p /etc/rool
 cp -f /tmp/rool/*.sh /etc/rool/
 chmod +x /etc/rool/*.sh
 
-echo "=== [5/8] Настройка задач cron ==="
+echo "[5/8] Настройка задач cron"
 CRON_FILE="/etc/crontabs/root"
 touch "$CRON_FILE"
 
@@ -43,18 +43,18 @@ if ! grep -q "/etc/rool/pog.sh" "$CRON_FILE"; then
     # Перезапуск службы cron для применения изменений
     /etc/init.d/cron restart
 else
-    echo "Задачи cron уже существуют, пропускаем."
+    echo "Задачи cron уже существуют, пропускаем"
 fi
 
-echo "=== [6/8] Установка темы Argon через apk ==="
+echo "[6/8] Установка темы Argon через apk"
 # В OpenWrt 25.xx используется apk
 apk add --no-cache --allow-untrusted /tmp/rool/argon.apk
 
-echo "=== [7/8] Очистка временной папки ==="
+echo "[7/8] Очистка временной папки"
 rm -rf /tmp/rool
 
-echo "=== [8/8] Установка podkop ==="
+echo "[8/8] Установка podkop"
 # Скачиваем и сразу запускаем установщик podkop
 sh <(wget -O - https://raw.githubusercontent.com/itdoginfo/podkop/refs/heads/main/install.sh)
 
-echo "=== Восстановление успешно завершено! ==="
+echo "Восстановление успешно завершено!"
